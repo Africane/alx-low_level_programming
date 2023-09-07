d500f007017b3ddcd5b0f8d17ef62093e8a0a018
David Owino <daveowino753@gmail.com>
2023-09-07 16:17:42 -0700

    added file to print text in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-	char myString[] = "Programming is like a building multilingual puzzle";
-
+	char myString[] = "Programming is like building a multilingual puzzle";
 	puts(myString);
-
 	return 0;
 }
