6c2338ed13e8d885978a93a40af8647c314b36d1
David Owino <daveowino753@gmail.com>
2023-09-07 16:35:07 -0700

    added comments to file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
+	//Display message on screen
 	char myString[] = "\"Programming is like building a multilingual puzzle";
 	puts(myString);
-	return 0;
+	return 0; //indicates successful execution
 }
