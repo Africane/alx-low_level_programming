27ec4d3912183154fc6b1eed39cf4b9e7e0ed98f
David Owino <daveowino753@gmail.com>
2023-09-07 16:44:56 -0700

    revoked changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 int main(void)
 {
-	//Display message on screen
 	char myString[] = "\"Programming is like building a multilingual puzzle";
 	puts(myString);
-	return 0; //indicates successful execution
+	return 0; 
 }
