69af827511efc0f12ef21c03fd7a91489509a0d9
David Owino <daveowino753@gmail.com>
2023-09-07 17:32:29 -0700

    amended file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/*
+ * main -Entry point
+ *
+ * Description: using puts to display text
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
-	char myString[] = "\"Programming is like building a multilingual puzzle";
-	puts(myString);
-	return 0; 
+char myString[] = "\"Programming is like building a multilingual puzzle";
+puts(myString);
+return 0; 
 }
