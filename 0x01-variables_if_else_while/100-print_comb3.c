c25abbc10f8919b63d95e485889c0f3d6c2f2df6
David Owino <daveowino753@gmail.com>
2023-09-08 14:09:50 -0700

    amended file to print possible combos

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,20 @@ int main(void)
 {
 	int digit, p;
 
-	for (digit = '0'; digit < '9'; digit++)
+	for (digit = '0'; digit <= '8'; digit++)
 	{
 		for (p = digit + 1; p <= '9'; p++)
-		{
-			if (p != digit)
+		{	
+			putchar('digit');
+			putchar('p');
+			if (digit != '8' || p != '9')
 			{
-				putchar(digit);
-				putchar(p);
-
-				if (digit == '0' && p == '9')
-					continue;
-
-				putchar(',');
-				putchar(' ');
+				putchar(,);
+				putchar( );
 			}
 		}
 	}
+	
 	putchar('\n');
 
 	return 0;
