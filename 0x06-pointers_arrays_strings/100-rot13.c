147e07efe785b62a76279c3015a6af0dc485e13d
David Owino <daveowino753@gmail.com>
2023-09-20 16:45:05 -0700

    amended file 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,21 +9,18 @@
 char *leet(char *str)
 {
 	char *ptr = str;
-	char leet_map[256] = {0}; /*initialize char mapping array*/
+	char *leet_chars = "aAeEoOtTlL";
+	char *leet_replacements = "4433007711";
 
-	/*initialize char mapping array for lowercase letters*/
-	leet_map['a'] = leet_map['A'] = '4';
-	leet_map['e'] = leet_map['E'] = '3';
-	leet_map['o'] = leet_map['O'] = '0';
-	leet_map['t'] = leet_map['T'] = '7';
-	leet_map['l'] = leet_map['L'] = '1';
-
-	/*first loop: iterate through string to apply encoding*/
 	while (*ptr)
 	{
-		if (leet_map[(unsigned char)*ptr]) /*check if char need encode*/
+		for (int i = 0; leet_chars[i]; i++)
 		{
-			*ptr = leet_map[(unsigned char)*ptr];
+			if (*ptr == leet_chars[i])
+			{
+				*ptr = leet_replacements[i];
+				break; /*Exit the loop once replaced*/
+			}
 		}
 		ptr++;
 	}
