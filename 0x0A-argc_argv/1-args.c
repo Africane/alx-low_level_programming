2284a9ac02a976dbed29d87db70c1fa9a9d41223
David Owino <daveowino753@gmail.com>
2023-09-29 09:24:26 -0700

    added file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argc: number of cli args
+ * @argv: array of strings containing cli args
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	/*print the value of argc*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
