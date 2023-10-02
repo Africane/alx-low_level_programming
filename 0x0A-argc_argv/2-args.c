d71fd5dbdf1681f2ce33723d0b7ae497ab249616
David Owino <daveowino753@gmail.com>
2023-10-02 02:46:29 -0700

    2nd amend to file 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int i;
 
 	/*loop through all cli args*/
-	for (int i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		/*print each arg followed by a new line*/
 		printf("%s\n", argv[i]);
