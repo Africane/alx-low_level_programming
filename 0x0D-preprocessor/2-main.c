3f11440f0428b78e21cfa938223de82182fe9905
David Owino <daveowino753@gmail.com>
2023-10-09 12:31:02 -0700

    added file 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * return: 0 if successful
+ */
+
+int main(void)
+{
+	printf("The name of the current file is: %s\n", __FILE__);
+	return (0);
+}
