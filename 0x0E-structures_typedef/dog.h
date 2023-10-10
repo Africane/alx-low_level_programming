5615f58632e524a12ad4cde383f00dd84c6e45d3
David Owino <daveowino753@gmail.com>
2023-10-09 19:21:18 -0700

    amended file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - represents information about a dog
- * @name: pointer to a char array
- * @age: age of dog (floating point num)
- * @owner: pointer to char array
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  */
 
 struct dog
@@ -14,18 +15,17 @@ struct dog
 	char *owner;
 };
 
-int main(void)
-{
-	/*Create an instance of struct dog and initialize its members*/
-	struct dog myDog;
-	myDog.name = "Putin";
-	myDog.age = 3.7;
-	myDog.owner = "Arsene Lupin";
+/**
+ * dog_t - typedef for struct dog
+ */
 
-	/*Access and print the information about the dog*/
-	printf("Name: %s\n", myDog.name);
-	printf("Age: %.2f years\n", myDog.age);
-	printf("Owner: %s\n", myDog.owner);
+typedef struct dog dog_t;
 
-	return (0);
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
