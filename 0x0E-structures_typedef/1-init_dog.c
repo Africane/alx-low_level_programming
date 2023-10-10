b1e33417417c6fa40980e4e5deb5bf3ec6a6aa8c
David Owino <daveowino753@gmail.com>
2023-10-09 19:24:41 -0700

    amended file 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,42 +1,18 @@
-#include <stdio.h>
-
-/*Define the struct dog type*/
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * init_dog - initializes a strcut dog variable with given values
- * @d: pointer to struct dog var to be initialized
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
-
-int main(void)
-{
-	struct dog myDog;
-
-	/*Initialize myDog using the init_dog function*/
-	init_dog(&myDog, "Buddy", 3.5, "John Doe");
-
-	/*Access and print the information about the dog*/
-	printf("Name: %s\n", myDog.name);
-	printf("Age: %.2f years\n", myDog.age);
-	printf("Owner: %s\n", myDog.owner);
-
-	return (0);
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
