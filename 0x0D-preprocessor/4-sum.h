17942cb5074756d4b8d838dfd77964cf9e53994a
David Owino <daveowino753@gmail.com>
2023-10-09 19:15:55 -0700

    amended file 4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,14 +1,6 @@
-#include <stdio.h>
+#ifndef SUM_H
+#define SUM_H
 
-/*Define the SUM macro*/
 #define SUM(x, y) ((x) + (y))
 
-int main(void)
-{
-	int a = 5;
-	int b = 7;
-	int result = SUM(a, b);
-
-	printf("The sum of %d and %d is ^Write a function-like macro SUM(x, y) that computes the sum of the numbers x and %d\n", a, b, result);
-	return (0);
-}
+#endif
