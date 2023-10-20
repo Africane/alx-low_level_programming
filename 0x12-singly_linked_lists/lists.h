bd3b300db9b020a6acdde479bed5cb45c7cb3520
David Owino <daveowino753@gmail.com>
2023-10-20 02:37:48 -0700

    added file 1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,4 +12,7 @@ typedef struct list_s
 /*Function prototype for print_list*/
 size_t print_list(const list_t *h);
 
+/*Function prototype for list_len*/
+size_t list_len(const list_t *h);
+
 #endif
