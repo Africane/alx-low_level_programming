c5fc8cd470a6c95fcfa94f8dde3e27f89794ce34
David Owino <daveowino753@gmail.com>
2023-11-15 05:59:22 -0800

    amended file 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,18 @@
+#include "main.h"
+
 /**
  * _puts_recursion - prints string followed by new line using recursion
  * @s: pointer to the string to be printed
- *
- * Return" on success, 0 on error -1
  */
-
-#include "main.h"
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return (0);
+		return;
 	}
 
-	_putchar(*s);
+	putchar(*s);
 	_puts_recursion(s + 1);
 }
