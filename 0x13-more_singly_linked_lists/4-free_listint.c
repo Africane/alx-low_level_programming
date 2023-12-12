7424af536640d74bccbe6b435db1e49eb062a184
David Owino <daveowino753@gmail.com>
2023-12-12 10:52:15 -0800

    added file 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp_memory;
+
+	while (head)
+	{
+		temp_memory = head->next;
+		free(head);
+		head = temp_memory;
+	}
+}
